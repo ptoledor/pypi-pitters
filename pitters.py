443b89a26b2df65b721dee8e1116e30a58994389
Pitters <ptoledor@msn.com>
2023-08-30 08:18:56 -0400

    Initial commit

---
+++ pitters.py
@@ -0,0 +1,4 @@
+import unicodedata
+
+def un_mensaje():
+    print('Hello')
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = "pitters",
+    version = "0.0.1",
+    author = "Pedro Toledo",
+    author_email = "ptoledor@msn.com",
+    description = "short package description",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ptoledor/pypi-pitters",
+    license="MIT",
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages = setuptools.find_packages(),
+    install_requires=[], 
+    python_requires = ">=3.6",
+)
